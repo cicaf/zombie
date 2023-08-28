64823100006cadc59b84910db2eed36f904fb646
o_w <womarow@gmail.com>
2023-08-29 01:06:12 +0300

    kins

--- Contract.sol
+++ Contract.sol
@@ -19,7 +19,27 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        // start here
+////////////////////////////////////////////* //MSG.SENDER ON MAPPING UPDATE
+	mapping (address => uint) favoriteNumber;
+
+function setMyNumber(uint _myNumber) public {
+  // Update our `favoriteNumber` mapping to store `_myNumber` under `msg.sender`
+  favoriteNumber[msg.sender] = _myNumber;
+  // ^ The syntax for storing data in a mapping is just like with arrays
+}
+
+function whatIsMyNumber() public view returns (uint) {
+  // Retrieve the value stored in the sender's address
+  // Will be `0` if the sender hasn't called `setMyNumber` yet
+  return favoriteNumber[msg.sender];
+}
+
+
+
+
+
+
+*/////////////////////////////////////////////////////////////////
         emit NewZombie(id, _name, _dna);
     }
 
